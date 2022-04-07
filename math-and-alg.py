601cf5d3448b162cd304bfd5ee8d5f2439205be5
daiki328 <daikisakuma6040@gmail.com>
2022-04-07 21:13:35 +0900

    today

--- math-and-alg.py
+++ math-and-alg.py
@@ -1,23 +1,30 @@
+# 001
 # N = int(input())
 # print(5+N)
 
+# 002
 # A1, A2, A3 = map(int, input().split())
 # print(A1+A2+A3)
 
+# 003
 # N = int(input())
 # A = list(map(int, input().split()))
 # print(sum(A))
 
+# 004
 # A1, A2, A3 = map(int, input().split())
 # print(A1*A2*A3)
 
+# 005
 # N = int(input())
 # a = list(map(int, input().split()))
 # print(sum(a)%100)
 
+# 006
 # N = int(input())
 # print(2*N+3)
 
+# 007
 # N, X, Y = map(int, input().split())
 # ans = 0
 # for i in range(1, N+1):
@@ -25,6 +32,7 @@
 #         ans += 1
 # print(ans)
 
+# 008
 # N, S = map(int, input().split())
 # ans = 0
 # for i in range(1, N+1):
@@ -33,6 +41,7 @@
 #       ans += 1
 # print(ans)
 
+# re: 009
 # N, S = map(int, input().split())
 # A = list(map(int, input().split()))
 # # 配列の初期化
@@ -58,3 +67,54 @@
 # else:
 # 	print("No")
 
+# 010
+# N = int(input())
+# ans = 1
+# for i in range(N, 1, -1):
+#   ans *= i
+# print(ans)
+
+# 011
+# N = int(input())
+# ans = []
+# for i in range(2, N+1):
+#   for j in range(2, i):
+#     if i % j == 0:
+#       break
+#   else:
+#     ans.append(i)
+# print(*ans)
+
+# 012
+# from math import sqrt
+# N = int(input())
+# ans = 0
+# for i in range(2, int(sqrt(N))+1):
+#   if N % i == 0:
+#     print('No')
+#     exit()
+# else:
+#   print('Yes')
+
+# 013
+# from math import sqrt
+# N = int(input())
+# ans = []
+# for i in range(1, int(sqrt(N))+1):
+#   if N % i == 0:
+#     ans.append(i)
+#     ans.append(int(N/i))
+# for i in ans:
+#   print(i)
+
+# re: 014
+from math import sqrt
+N = int(input())
+ret = []
+for i in range(2, int(sqrt(N))+1):
+  while N % i == 0:
+    ret.append(i)
+    N //= i
+if N != 1:
+  ret.append(N) 
+print(*ret)
