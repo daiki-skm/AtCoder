0c85146ae32d21fbba0b9346e79360256adf6b0c
daiki328 <dk08sel11@i.softbank.jp>
2022-05-08 09:15:33 +0900

    5/8

---
+++ abc198.py
@@ -0,0 +1,25 @@
+# A
+# n = int(input())
+# print(n-1)
+
+# B
+# S = input()
+# for i in range(10):
+# 	T = "0"*i + S
+# 	if T==T[::-1]:
+# 		print("Yes")
+# 		exit()
+# print("No")
+
+# C
+# r, x, y = map(int, input().split())
+# d2 = x**2 + y**2
+# ans = 1
+# while True:
+#   if r**2*ans**2 >= d2:
+#     break
+#   ans += 1
+# if ans == 1:
+#   if r**2 != d2:
+#     ans = 2
+# print(ans)
