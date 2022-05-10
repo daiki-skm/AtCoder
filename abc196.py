4678a5832346c43b8cc26df84e2e5a15c6edce91
daiki328 <dk08sel11@i.softbank.jp>
2022-05-10 14:31:02 +0900

    5/10

---
+++ abc196.py
@@ -0,0 +1,22 @@
+# A
+# a, b = map(int, input().split())
+# c, d = map(int, input().split())
+# print(b-c)
+
+# B
+# x = input()
+# if '.' in x:
+#     id = x.index('.')
+#     print(x[:id])
+# else:
+#     print(x)
+
+# C
+# n = int(input())
+# ans = 0
+# for i in range(1, n):
+#     t = str(i)+str(i)
+#     if int(t) > n:
+#         break
+#     ans += 1
+# print(ans)
