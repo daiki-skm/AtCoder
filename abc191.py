13dc3680075c99c21a30dc0ae297f2e677cde1ce
daiki328 <dk08sel11@i.softbank.jp>
2022-05-16 10:05:11 +0900

    Changes

---
+++ abc191.py
@@ -0,0 +1,12 @@
+# A
+# v, t, s, d = map(int, input().split())
+# if v*t <= d <= v*s:
+#     print("No")
+# else:
+#     print("Yes")
+
+# B
+# n, x = map(int, input().split())
+# a = list(map(int, input().split()))
+# a = [i for i in a if i != x]
+# print(*a)
