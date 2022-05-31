2950674505a49447b3aa67a3b8b6612843caec10
daiki328 <dk08sel11@i.softbank.jp>
2022-05-31 12:14:32 +0900

    5/31

---
+++ abc178.py
@@ -0,0 +1,19 @@
+# A
+# x = int(input())
+# if x == 0:
+#     print(1)
+# else:
+#     print(0)
+
+# B
+# a, b, c, d = map(int, input().split())
+# t1 = a * c
+# t2 = b * d
+# t3 = a * d
+# t4 = b * c
+# ans = max(t1, t2, t3, t4)
+# print(ans)
+
+# C
+# n = int(input())
+# print((10 ** n - 9 ** n - 9 ** n + 8 ** n) % (10 ** 9 + 7))
