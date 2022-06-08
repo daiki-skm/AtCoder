9bb24b6ef5a670d9ddc3acfd07986e828a4b7705
daiki328 <dk08sel11@i.softbank.jp>
2022-06-08 10:23:59 +0900

    6/8

---
+++ abc172.py
@@ -0,0 +1,12 @@
+# A
+# a = int(input())
+# print(a + a ** 2 + a ** 3)
+
+# B
+# s = input()
+# t = input()
+# ans = 0
+# for i in range(len(s)):
+#     if s[i] != t[i]:
+#         ans += 1
+# print(ans)
