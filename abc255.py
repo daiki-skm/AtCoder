8258d69211419e45c19f074cff5ea5fb240691cd
daiki328 <dk08sel11@i.softbank.jp>
2022-06-13 07:14:09 +0900

    6/13

--- abc255.py
+++ abc255.py
@@ -2,18 +2,19 @@
 # r, c = map(int, input().split())
 # a11, a12 = map(int, input().split())
 # a21, a22 = map(int, input().split())
+# ans = 0
 # if r == 1 and c == 1:
-#     print(a11)
-# elif r == 1 and c == 2:
-#     print(a12)
-# elif r == 2 and c == 1:
-#     print(a21)
-# elif r == 2 and c == 2:
-#     print(a22)
+#     ans = a11
+# if r == 1 and c == 2:
+#     ans = a12
+# if r == 2 and c == 1:
+#     ans = a21
+# if r == 2 and c == 2:
+#     ans = a22
+# print(ans)
 
 # B
 # from math import sqrt
-# from collections import defaultdict
 #
 # n, k = map(int, input().split())
 # a = list(map(int, input().split()))
@@ -22,19 +23,52 @@
 # y = [0] * n
 # for i in range(n):
 #     x[i], y[i] = map(int, input().split())
-# ans = defaultdict(list)
-# for j in range(n):
-#     for k_ in range(j + 1, n):
-#         if not j in a and k_ in a or not k_ in a and j in a:
-#             t = abs(x[j] - x[k_]) ** 2 + abs(y[j] - y[k_]) ** 2
-#             ans[j].append(t)
-#             ans[k_].append(t)
-# t = 0
-# for i in ans:
-#     if i in a:
-#         continue
-#     t = max(t, min(ans[i]))
-# print(sqrt(t))
+# r = [0] * n
+# for i in range(n):
+#     r[i] = 10 ** 18
+#     for j in range(k):
+#         dist = sqrt((x[i] - x[a[j]]) ** 2 + (y[i] - y[a[j]]) ** 2)
+#         r[i] = min(r[i], dist)
+# ans = 0
+# for i in range(n):
+#     ans = max(ans, r[i])
+# print(ans)
 
 # C
-x, a, d, n = map(int, input().split())
+# x, a, d, n = map(int, input().split())
+# if d == 0:
+#     print(abs(x - a))
+#     exit()
+# if d < 0:
+#     a = a + d * (n - 1)
+#     d = -d
+# i = (x - a) // d
+#
+#
+# def f(i):
+#     if i < 0:
+#         i = 0
+#     if i >= n:
+#         i = n - 1
+#     return a + i * d
+#
+#
+# ans = abs(x - f(i))
+# ans = min(ans, abs(x - f(i + 1)))
+# print(ans)
+
+# D
+# from bisect import bisect_left
+#
+# n, q = map(int, input().split())
+# a = list(map(int, input().split()))
+# a.sort()
+# s = [0] * (n + 1)
+# for i in range(n):
+#     s[i + 1] = s[i] + a[i]
+# for i in range(q):
+#     x = int(input())
+#     k = bisect_left(a, x)
+#     ans = k * x - s[k]
+#     ans += s[n] - s[k] - (n - k) * x
+#     print(ans)
