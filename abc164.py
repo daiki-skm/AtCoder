c7dbcb0e4f9b55a63cc3f7759454738ebfb78d75
daiki328 <dk08sel11@i.softbank.jp>
2022-06-17 08:32:53 +0900

    6/17

---
+++ abc164.py
@@ -0,0 +1,22 @@
+# A
+# s, w = map(int, input().split())
+# if w >= s:
+#     print('unsafe')
+# else:
+#     print('safe')
+
+# B
+# a, b, c, d = map(int, input().split())
+# t1 = (a + d - 1) // d
+# t2 = (c + b - 1) // b
+# if t1 >= t2:
+#     print('Yes')
+# else:
+#     print('No')
+
+# C
+# n = int(input())
+# s = set()
+# for i in range(n):
+#     s.add(input())
+# print(len(s))
