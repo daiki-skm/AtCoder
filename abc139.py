64cd9c02a846895da93c8694e702abd270e31765
daiki328 <dk08sel11@i.softbank.jp>
2022-07-17 10:09:49 +0900

    7/17

---
+++ abc139.py
@@ -0,0 +1,30 @@
+# A
+# s = input()
+# t = input()
+# ans = 0
+# for i in range(3):
+#     if s[i] == t[i]:
+#         ans += 1
+# print(ans)
+
+# B
+# a, b = map(int, input().split())
+# print((b + a - 3) // (a - 1))
+
+# C
+# n = int(input())
+# h = list(map(int, input().split()))
+# h.reverse()
+# ans = 0
+# val = 0
+# for i in range(1, n):
+#     if h[i - 1] <= h[i]:
+#         val += 1
+#     else:
+#         val = 0
+#     ans = max(ans, val)
+# print(ans)
+
+# D
+# n = int(input())
+# print((n - 1) * n // 2)
